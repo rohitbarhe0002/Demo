5696ad0e681949069faf5a22c53aff0952e907ee
rohit barche <rohitbarche99@gmail.com>
2021-11-24 20:11:26 +0530

    change in app

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          dimag la bhosda
         </a>
       </header>
     </div>
